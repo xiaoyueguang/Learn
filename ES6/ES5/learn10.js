ad8c664089b5035069b9744a029a110d58523b70
linjilei <linjilei@Y450.com>
2016-08-15 23:22:13 +0800

    proxy 部分

--- ES6/ES5/learn10.js
+++ ES6/ES5/learn10.js
@@ -16,3 +16,84 @@
     console.log(proxy.name);
     console.log(proxy.age);
 }
+
+{
+    var person = {
+        name: "张三"
+    };
+
+    var _proxy = new Proxy(person, {
+        get: function get(target, property) {
+            if (property in target) {
+                return '拦截到的name为' + property + ";拦截到的值为" + target[property];
+            } else {
+                throw new ReferenceError('Error');
+            }
+        }
+    });
+
+    console.log(_proxy.name);
+}
+
+{
+    var validator = {
+        set: function set(obj, prop, value) {
+            console.log(obj, prop, value);
+            if (prop === 'age') {
+                if (typeof value !== 'number') {
+                    throw new TypeError('This is not a number!');
+                }
+                if (value > 100) {
+                    throw new RangeError('Age Input Error!');
+                }
+                obj[prop] = value;
+            }
+        }
+    };
+
+    var _person = new Proxy({}, validator);
+
+    _person.age = 12; //  {age: 12}
+    perspn.age = '12'; //  => Error
+    _person.age = 111; //  => Error
+}
+
+{
+    var target = function target() {
+        return 'I am the target';
+    };
+    var handler = {
+        apply: function apply() {
+            return 'I am the proxy';
+        }
+    };
+
+    var p = new Proxy(target, handler);
+
+    p();
+    p.call();
+    p.apply();
+}
+
+{
+    var _person2 = {
+        name: 'HanMei',
+        age: 18
+    };
+
+    var _handler = {
+        has: function has(target, prop) {
+            if (prop === 'age') {
+                return false;
+            }
+            return prop in target;
+        }
+    };
+
+    var _proxy2 = new Proxy(_person2, _handler);
+
+    'name' in _proxy2;
+    'age' in _proxy2;
+}
+
+{}
--- ES6/ES6/learn10.js
+++ ES6/ES6/learn10.js
@@ -12,3 +12,82 @@
     console.log(proxy.name);
     console.log(proxy.age);
 }
+
+{
+    const person = {
+        name: "张三"
+    };
+
+    const proxy = new Proxy(person, {
+        get: function(target, property) {
+            if (property in target) {
+                return '拦截到的name为' + property + ";拦截到的值为" + target[property];
+            } else {
+                throw new ReferenceError('Error');
+            }
+        }
+    });
+
+    console.log(proxy.name);
+}
+
+{
+    const validator = {
+        set: (obj, prop, value) => {
+            console.log(obj, prop, value);
+            if (prop === 'age') {
+                if (typeof value !== 'number') {
+                    throw new TypeError('This is not a number!');
+                }
+                if (value > 100) {
+                    throw new RangeError('Age Input Error!')
+                }
+                obj[prop] = value;
+            }
+        }
+    };
+
+    const person = new Proxy({}, validator);
+
+    person.age = 12;    //  {age: 12}
+    perspn.age = '12';  //  => Error
+    person.age = 111;   //  => Error
+}
+
+{
+    const target = () => 'I am the target';
+    const handler = {
+        apply: () => 'I am the proxy'
+    };
+
+    let p = new Proxy(target, handler);
+
+    p();
+    p.call();
+    p.apply();
+}
+
+{
+    const person = {
+        name: 'HanMei',
+        age: 18
+    };
+
+    const handler = {
+        has (target, prop) {
+            if (prop === 'age') {
+                return false;
+            }
+            return prop in target;
+        }
+    }
+
+    const proxy = new Proxy(person, handler);
+
+    'name' in proxy;
+    'age' in proxy;
+}
+
+{
+    
+}
