a3041eeb73e12d6198db0a705215f4de9c17e367
linjilei <linjilei@Y450.com>
2017-05-21 19:26:00 +0800

    redux

--- learn-react/src/Flux/redux.js
+++ learn-react/src/Flux/redux.js
@@ -1,6 +1,6 @@
 // 引用中间件 applyMiddleware
 import {createStore, applyMiddleware} from 'redux'
-import promiseMiddleware from 'redux-promise'
+// import promiseMiddleware from 'redux-promise'
 // import {createLogger} from 'redux-logger'
 // const logger = createLogger()
 
@@ -31,8 +31,15 @@ const reducer = (state = defaultState, action) => {
           count2: state.count2 + 1
         }
       }
+    case 'DOWN':
+      return {
+        ...state,
+        ...{
+          count2: state.count2 - 1
+        }
+      }
     case 'PLUS':
-      console.log(action)
+      console.log('PLUS', action)
       return {...state, ...action.payload}
     default:
       return state
@@ -46,23 +53,55 @@ const reducer = (state = defaultState, action) => {
  */
 const log = store => next => action => {
   if (action.type === 'PLUS') {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        // resolve({...action, ...{
-        //   payload: {
-        //     count2: store.getState().count2 + 1
-        //   }
-        // }})
-        resolve({type: 'ADD2'})
-      }, 1000)
-    }).then(action => next(action))
-    // if (count2 === 10) {
-    //   return next(action)
-    // } else {
+    console.log('log1', action)
+    // return new Promise((resolve, reject) => {
     //   setTimeout(() => {
-    //     next(action)
+    //     resolve({type: 'ADD2'})
+    //   }, 200)
+    // }).then(() => {
+    //   return new Promise((resolve, reject) => {
+    //     setTimeout(() => {
+    //       resolve({type: 'ADD2'})
+    //     }, 300)
     //   })
-    // }
+    // })
+    // 只有执行了 next. 中间件才会将数据传递给下一个中间件
+    // 在一个中间件中可执行多个next. 并不冲突
+    // 当有中间件的时候
+    // .then(action => next(action))
+    setTimeout(() => {
+      next({type: 'ADD2'})
+    }, 1000)
+    // 可在
+    setTimeout(() => {
+      next({type: 'DOWN'})
+    }, 2000)
+    return next({...action, ...{log1: 'log1中的数据'}})
+  }
+  return next(action)
+}
+
+const log2 = store => next => action => {
+  if (action.type === 'PLUS') {
+    console.log('log2', action)
+    setTimeout(() => {
+      next({...action, ...{log2: 'log2中的数据'}})
+    }, 1000)
+    // 返回可为空 或者 Promise. 可延迟将数据传入下个中间件
+    // 当 return 为空时, 或者没有 next时会导致数据 redux 停留到当前中间件.
+    return
+  }
+  return next(action)
+}
+
+const log3 = store => next => action => {
+  if (action.type === 'PLUS') {
+    return new Promise(resolve => {
+      console.log('log3', action)
+      setTimeout(() => {
+        resolve()
+      }, 1000)
+    }).then(() => next({...action, ...{log3: 'log3中的数据'}}))
   }
   return next(action)
 }
@@ -70,7 +109,8 @@ const log = store => next => action => {
 const store = createStore(reducer,
   // 触发 调试工具
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(log)
+  // 中间件 会按照 applyMiddleware 中传入的顺序执行
+  applyMiddleware(log, log3, log2)
 );
 
 export default store
