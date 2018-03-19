76bf2ced91d1a8fca1a4788dc27d90361af2f346
linjilei <linjilei@qq.com>
2018-03-19 11:56:00 +0800

    services worker 添加注释

--- sw/sw.js
+++ sw/sw.js
@@ -1,29 +1,49 @@
-self.addEventListener('install', function(event) {
+this.addEventListener('install', function (event) {
+  console.log('安装完成')
   event.waitUntil(
-    caches.open('v2').then(function(cache) {
+    caches.open('v1').then(function (cache) {
       return cache.addAll([
-        '/index.html'
-      ]);
+        // '/index.html'
+      ])
     })
-  );
-});
+  )
+})
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(caches.match(event.request).then(function(response) {
-    if (response !== undefined) {
-      return response;
-    } else {
-      return fetch(event.request).then(function (response) {
-        let responseClone = response.clone();
-        
-        caches.open('v2').then(function (cache) {
-          console.log(event.request)
-          cache.put(event.request, responseClone);
-        });
-        return response;
-      }).catch(function () {
-        return caches.match('/1.jpg');
-      });
-    }
-  }));
-});
+this.addEventListener('activate', function (event) {
+  // console.log('activate', event)
+  console.log('激活完成')
+})
+
+// FetchEvent 请求头事件. 挟持浏览器请求事件
+this.addEventListener('fetch', function (event) {
+  const REQUEST = event.request.clone()
+  REQUEST.url = event.request.url.replace(/[&?]v=[\d]*/, '')
+
+  event.respondWith(
+    // response 响应头, 包含状态码 链接 类型 数据等
+    // match 从cache中, 抓取数据. 如果抓到了, 则返回响应数据
+    caches.match(REQUEST).then(function (response) {
+    // 抓取到则返回数据
+      if (response !== undefined) {
+        return response
+      } else {
+        console.log(REQUEST)
+        // 没抓取到则调用浏览器默认请求, 并且做缓存
+        return fetch(REQUEST).then(function (response) {
+          // 复制一份
+          let responseClone = response.clone()
+          // console.log(responseClone)
+          // 打开对应的版本库
+          caches.open('v1').then(function (cache) {
+            // cache 将请求和响应, 同时放进缓存
+            cache.put(REQUEST, responseClone)
+          })
+          return response
+        }).catch(function () {
+          return caches.match('')
+        })
+
+      }
+    })
+  )
+})
