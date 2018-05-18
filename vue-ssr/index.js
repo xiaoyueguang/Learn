bcfcc209fdb22e8c11a9e44a90893747281bfa74
linjilei <linjilei@qq.com>
2018-05-18 16:56:43 +0800

    添加 express 渲染

--- vue-ssr/index.js
+++ vue-ssr/index.js
@@ -1,14 +1,35 @@
 const Vue = require('vue')
-// 创建一个实例, 这里跟浏览器端的行为是一致的.
-const app = new Vue({
-  template: `<div>Hello World</div>`
+const server = require('express')()
+const createRenderer = require('vue-server-renderer').createRenderer
+
+const renderer = createRenderer({
+  // 读取模板
+  template: require('fs').readFileSync('./index.template.html', 'utf-8')
 })
 
-// 创建一个renderer, 专门负责将传入的vue实例转换为字符串
-const renderer = require('vue-server-renderer').createRenderer()
-// 将实例转换为一个html字符串
-renderer.renderToString(app, (err, html) => {
-  if (err) throw err
-  console.log(html)
-  // <div data-server-rendered="true">Hello World</div>
+server.get('*', (req, res) => {
+  const app = new Vue({
+    data () {
+      return {
+        url: req.hostname
+      }
+    },
+    template: `
+      <div>
+        <span>Hello SSR FROM <em>{{url}}</em>!</span>
+        <p><span v-for="i in 9" :key="i">{{i}}</span></p>
+      </div>
+    `
+  })
+
+  renderer.renderToString(app, (err, html) => {
+    if (err) {
+      res.status(500).end('Internal Server Error')
+      return
+    }
+    // 生成 html 后, 在这里结束响应并返回
+    res.end(html)
+  })
 })
+
+server.listen(8080)
