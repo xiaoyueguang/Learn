6a678c5f1f13a2d9266c31d164eadb2e728a7be3
linjilei <linjilei@qq.com>
2020-08-09 17:16:09 +0800

    移除文件

--- karma/test/subtract.Spec.js
+++
@@ -1,8 +0,0 @@
-import {subtract} from '../src/subtract'
-import {expect} from 'chai'
-
-describe('subtract', function () {
-  it('subtract(5, 4) === 1', function () {
-    expect(subtract(5, 4)).to.be.equal(1)
-  })
-})
